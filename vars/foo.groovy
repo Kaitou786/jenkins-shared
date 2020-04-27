4db06daec4430cb7809d9ff8fbca7c06ac4cecf7
Tarun Khandelwal <khandelwaltarun32@live.com>
2020-04-27 11:16:17 +0530

    Update foo.groovy

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -1,1 +1,11 @@
 #here sample pipeline will execute
+def call(){
+  pipeline{
+    agent {label 'jenkins-slave'}
+    stages{
+      stage('Sample stage'){
+        sh "I am called from a shared pipeline"
+      }
+    }
+  } 
+}
